1b088147beeeff28c2780b53db99c5994debff7d
Julien Woillez <jwoillez@eso.org>
2018-10-20 21:25:33 +0000

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name = 'pyNaomi',
+      version = '1.0',
+      description = 'Python tools for NAOMI',
+#      package_dir = {'':'lib'},
+#      packages = find_packages('lib'),
+      scripts = ['scripts/modal_disturbance.py'])
